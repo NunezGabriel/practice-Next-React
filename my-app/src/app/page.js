897257d60fe02b68081ef6af6fb546e358a5ee1f
Gabriel <emilio.nunez.arenas@gmail.com>
2023-08-25 20:47:07 -0500

    starting a new pproyect

---
+++ my-app/src/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <h1> this is a list</h1>
+      <select name="select">
+        <option value="value1">Value 1</option>
+        <option value="value2" selected>
+          Value 2
+        </option>
+        <option value="value3">Value 3</option>
+      </select>
+    </>
+  );
+}
