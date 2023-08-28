4164877e2e328da2fa832023b3901ebe17ea0f18
Gabriel <emilio.nunez.arenas@gmail.com>
2023-08-27 20:57:51 -0500

    adding more changes

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,14 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1> this is a list</h1>
-      <select name="select">
-        <option value="value1">Value 1</option>
-        <option value="value2" selected>
-          Value 2
-        </option>
-        <option value="value3">Value 3</option>
-      </select>
+      <div className="mx-auto rounded-lg mt-12 text-center p-5 border-[3px] border-green-400 max-w-[500px] bg-gray-800">
+        <h1 className="text-2xl text-white">WELCOME TO THIS PRACTICE</h1>
+        <p className="text-green-700 text-xl">
+          please click any link to see a simple exercise solved
+        </p>
+      </div>
     </>
   );
 }
---
+++ my-app/src/app/useState/page.jsx
@@ -0,0 +1,9 @@
+const UseState = () => {
+  return (
+    <>
+      <h1 className="text-white">USE STATE PRACTICE</h1>
+    </>
+  );
+};
+
+export default UseState;
